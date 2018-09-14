415723b6c11b8582e85f174abd18bafb2985d2a8
hityujiaming <43257692+hityujiaming@users.noreply.github.com>
2018-09-14 13:32:51 +0800

    Update JDBConnection.java

--- src/com/wy/tool/JDBConnection.java
+++ src/com/wy/tool/JDBConnection.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class JDBConnection {
 	private String dbDriver = "com.mysql.jdbc.Driver"; // ���ݿ������
 
-	private String url = "jdbc:mysql://localhost:3306/db_shopping"; // URL��ַ
-	//private String url = "jdbc:mysql://47.75.7.46:3306/db_shopping";
+	private String url = "jdbc:mysql://xxxxx/db_shopping"; // URL��ַ
+	//private String url = "jdbc:mysql://xxxxxxx/db_shopping";
 
 	public Connection connection = null;
 
@@ -14,7 +14,7 @@ public class JDBConnection {
 		try {
 			Class.forName(dbDriver).newInstance(); // �������ݿ�����
 			System.out.println("kkkkkkkkkkkkkkkkkkk00000");
-			connection = DriverManager.getConnection(url, "root", "iloveshark77"); // �������ݿ�
+			connection = DriverManager.getConnection(url, "xxx", "xxxxxx"); // �������ݿ�
 			System.out.println("kkkkkkkkkkkkkkkkkkk11111"+connection);
 		} catch (Exception ex) {
 			System.out.println("connection error by yu");
